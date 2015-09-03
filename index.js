3250cdea06c0e75e85e46fd2606b956b1ce38334
Alair JT <alair.tavares@nexxera.com>
2015-09-03 17:49:58 -0300

    Rename

---
+++ index.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+    
+    var chalk = require('chalk');
+    
+    var nexxSay = function () {
+        return "" +
+"\n                               ```                ``    " +
+"\n                                `-/+/-`          '.-    " +
+"\n                                  `-+so:.     .:.       " +
+"\n  .-:--.`                            `:oys/../:`        " +
+"\n `.:/++//:.`                           .+hdh/           " +
+"\n     `.:+ooo+/-.                       `-/++yhs-        " +
+"\n          `:+sssso/-`                 `-/+/. `/yh+`     " +
+"\n             `-+syyys+-`            `-/++-`    -ohy-    " +
+"\n                `:oyhyys/-        `:/++/.        .+os-  " +
+"\n                   ./shhhys/.   ./++oo:`          .+++  " +
+"\n                     `-oyyhhy+:/++os+.              .-. " +
+"\n                        .+syhmmdyss:`                   " +
+"\n                          :hddmmNd/         Bem vindo ao " + chalk.red("nxHiveWebApp") +
+"\n                        .+ssydmmhyyo.                   " +
+"\n                     -+sssyyyoosyyys/`                  " +
+"\n                   `-+yyyyyyo- `/osyyyo.                " +
+"\n                `-oyyyyyys/`    -+osyys:                " +
+"\n              `-oyyyyyyyo.       ./+osyy/`              " +
+"\n            `:oyhhyyyys:`         ./++osy+`             " +
+"\n          `:oyhhyyyyy+.            ./+++sy+             " +
+"\n        `:syhhyyyyys:               .++++sy/            " +
+"\n      `:shhhyyyyys+.                 :++++oo.           " +
+"\n    `/syhhhyyyyys-                    .---.`            " +                                                                                                                                                                                                      
+"\n   :syyssooooo+:`                                       " +                                                                                                                                                                                                      
+"\n   -++++////:.`  " +
+"\n\n";
+    };
+
+    module.exports = {nexxSay: nexxSay};
+})();
