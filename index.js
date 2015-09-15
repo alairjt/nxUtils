af08fdad955f531304a39c1c9ed0922403eaae05
Alair JT <alair.tavares@nexxera.com>
2015-09-15 09:20:47 -0300

    add utilities

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 (function () {
     'use strict';
     
-    var chalk = require('chalk');
+    var chalk = require('chalk'),
+        fs = require('fs'),
+        path = require('path');
     
     var nexxSay = function (appName) {
         return "" +
@@ -32,6 +34,52 @@
 "\n   -++++////:.`  " +
 "\n\n";
     };
+    
+    var getDirectories = function () {
+        var dir = path.dirname(process.cwd());
+        
+        var apps = [];
+        
+        fs.readdirSync(dir).filter(function(app) {
+            var file = path.join(dir, app) + "/config/Gruntfile.js";
+            if (fs.existsSync(file)) {
+                apps.push(app);
+            }
+        });
+        
+        
+        return apps;
+    };
+    
+    var getConfigs = function () {
+        var apps = getDirectories(),
+            configs = [];
+        
+        for (var key in apps) {
+            configs.push("../" + apps[key] + "/config/Gruntfile.js");
+        }
+        
+        return configs;
+    };
+    
+    var getConnects = function (connect) {
+        var apps = getDirectories(),
+            connects = [require('grunt-contrib-livereload/lib/utils').livereloadSnippet];
+        
+        for (var key in apps) {
+            connects.push(connect().use(
+                '/' + apps[key],
+                connect.static('../' + apps[key] + '/src')
+            ));
+        }
+        
+        return connects;
+    };
 
-    module.exports = {nexxSay: nexxSay};
+    module.exports = {
+        nexxSay: nexxSay,
+        getDirectories: getDirectories,
+        getConfigs: getConfigs,
+        getConnects: getConnects
+    };
 })();
